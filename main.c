15b43c84c8d207a695a3838cf4be936075072c2f
Alexandre Souto <soutoinf@gmail.com>
2023-05-23 22:01:13 -0300

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+#define F_CPU 16000000UL
+#define LED_PIN 0
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <FreeRTOS>
+
+int main()
+{
+	DDRB |= (1 << LED_PIN); // set pin as output
+    while (1) 
+    {
+		PORTB ^= (1 << LED_PIN); // toggles  pin		
+		_delay_ms(500); // busy wait, 500ms
+    }
+
+    return 0;
+}
